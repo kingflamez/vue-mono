e843963b93853a50ea44cdb2018d581dbeccf2bf
Flamez - Oluwole Adebiyi <flamekeed@gmail.com>
2020-08-08 17:55:15 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const MonoJS = {
+    install(Vue, {publicKey}) {    
+        Vue.mixin({
+            mounted() {
+                const monoJS = 'https://connect.withmono.com/connect.js'
+                const script = document.createElement('script')
+                script.src = monoJS
+                // Only run if mono script has not been added to the body
+                if (!document.querySelector(`[src="${monoJS}"]`)) {
+                    document.body.appendChild(script)
+                }
+            },
+          
+            methods: {
+                $launchMono(options) {
+                    const connect = new Connect(publicKey, options);
+                    connect.setup();
+                    connect.open()
+                },
+            }
+        })
+    }
+}
+
+export default MonoJS
